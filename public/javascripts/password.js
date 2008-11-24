ad826b59c1ef531611d9190c3e056e31d0b2b6b1
uipoet <dont.tase@me.com>
2008-11-23 16:41:27 -0800

    Updated README with current installation instructions. Moved jquery keyboard shortcuts to merb-ui.

--- public/javascripts/password.js
+++
@@ -1,8 +0,0 @@
-$(document).keyup(function(event){
-  if (event.keyCode == 119) {
-    windowOpen('/password/read');
-  }
-  else if (event.keyCode == 118) {
-    window.location = '/password/exit';
-  }
-});
